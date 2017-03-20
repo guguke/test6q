0d322e85a2958910f34eb723bb51f41084ffe3fb
gbm618 <gbm618@gmail.com>
2017-03-20 16:01:52 +0800

    max7359 driver

---
+++ key/cpk.bat
@@ -0,0 +1,7 @@
+#
+#cp board-mx6q_sabresd.c mach-mx6
+#cp board-mx6q_sabresd.h mach-mx6
+#cp spi_imx.c linux/drivers/spi/
+#cp spi.h linux/arch/arm/plat-mxc/include/mach/
+
+#cp max7359_keypad.c linux/drivers/input/keyboard/max7359_keypad.c
