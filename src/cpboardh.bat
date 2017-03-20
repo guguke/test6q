696d49f6b504da8507a96151d81403324309369e
gbm618 <gbm618@gmail.com>
2017-03-20 17:59:11 +0800

    cp board.h

--- src/cpboardh.bat
+++ src/cpboardh.bat
@@ -1,5 +1,5 @@
 #
-#cp board-mx6q_sabresd.c mach-mx6
-cp board-mx6q_sabresd.h mach-mx6
+cp board-mx6q_sabresd.c mach-mx6
+#cp board-mx6q_sabresd.h mach-mx6
 #cp spi_imx.c linux/drivers/spi/
 #cp spi.h linux/arch/arm/plat-mxc/include/mach/
